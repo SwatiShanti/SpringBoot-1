c0b69d8653c6dfe0e70307bce23798ea257c263f
{swaTI} <{swati}>
2026-07-14 11:23:40 +0530

    My Self

--- demo/src/main/java/com/swati/demo/Portfolio/MyPortfolio.java
+++ demo/src/main/java/com/swati/demo/Portfolio/MyPortfolio.java
@@ -5,16 +5,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyPortfolio {
-
-    @GetMapping("/MySelf")
-    public String MySelf(){
+    @GetMapping("/myself")
+    public String Myself() {
         return """
-                <h1>MySelf Swati</h1>
-                <p> I am a quick learner Adaptable</p>
+                <h1>Myself</h1>
+                
+                <p>Hi, I'm Swati Shanti!</p>
+                
+                <p>
+                Aspiring Cloud Computing and AI Enthusiast.<br>
+                Passionate about building web applications, cloud technologies,
+                and solving real-world problems.
+                </p>
+                
                 <ul>
-                <li>https://github.com/SwatiShanti</li>
-                <li>https://leetcode.com/u/swatishanti/</li>
+                    <li>GitHub: https://github.com/SwatiShanti</li>
+                    <li>LinkedIn: https://www.linkedin.com/in/swati-shanti12</li>
+                    <li>LeetCode: https://leetcode.com/u/swatishanti/</li>
+                </ul>
                 """;
-
     }
 }
