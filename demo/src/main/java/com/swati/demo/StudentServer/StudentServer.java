0425bf6a05083030a3c58e0b38910774e4724b2e
{swaTI} <{swati}>
2026-07-14 15:33:26 +0530

    StudentServer

--- demo/src/main/java/com/swati/demo/StudentServer/StudentServer.java
+++ demo/src/main/java/com/swati/demo/StudentServer/StudentServer.java
@@ -5,29 +5,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-
+@RestController // to listen http incoming request
 public class StudentServer {
+    // 1. Store the Student - post http method
+//    @PostMapping("/create")
+//    public String storeStudent(@RequestBody Student student) {
+//        int id = student.getId();
+//        String name = student.getName();
+//        int age = student.getAge();
+//        String department = student.getDepartment();
+//
+//        return "id: " +id +
+//                ", name: " +name +
+//                ", age: " +age +
+//                ", department: " +department;
+//    }
+
+//        return """
+//                id : 1,
+//                name : Palak,
+//                Department : CSE,
+//                age : 20
+//                """;
 
 
-//    1.Store the Student
-    @PostMapping("/create")
-    public String storeStudent(@RequestBody Student student){
-        int id = student.getId();
-        String name = student.getName();
-        int age = student.getAge();
-        String department = student.getDepartment();
-
-        return "id: " +id +
-                ", name: "+name +
-                ", age: "+age +
-                ", department: "+department;
-
-    }
-//    2.Read the Student with id
+    // 2. Read the student with id - get method
 
 
-//    3.update the student information
-//    4.delete the student information
+    // 3. Update the student information - put , patch http method
+
+    //4. delete the student information - delete
+
+
 
 }
